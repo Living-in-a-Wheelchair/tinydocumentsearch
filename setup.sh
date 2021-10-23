dda0dc99f225893fb89f46df61a6fdc1dbe7129f
Arman Bhalla <armanbhalla6@gmail.com>
2021-10-24 00:42:39 +0100

    Fix Tika installer

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # Requirements for this script to run:
 # Bash, Rust/Rustup, Cargo, Node/NPM, Java (JDK/JRE) 8
 
-curl -o tika.jar https://www.apache.org/dyn/closer.lua/tika/2.1.0/tika-app-2.1.0.jar
+curl -o tika-app-2.1.0.jar https://dlcdn.apache.org/tika/2.1.0/tika-app-2.1.0.jar
 
 cargo install wasm-pack
 npm install terser
