1cce8039b6abf0fb077132b20988925b9dc6b984
Arman Bhalla <armanbhalla6@gmail.com>
2021-10-24 00:43:12 +0100

    Fixed reference to Apache Tika

--- parse.sh
+++ parse.sh
@@ -2,4 +2,4 @@
 
 echo "Before running this program, the documents should be stored in the ./in_docs folder after running ./setup.sh. Please note that the parsing software will preserve the original directory sturcture."
 
-java -jar tika-app.jar -J -z --extract-dir=./out_tkparse -v  --inputDir=./in_docs --outputDir=./out_tkparse
+java -jar tika-app-2.1.0.jar -J -z --extract-dir=./out_tkparse -v  --inputDir=./in_docs --outputDir=./out_tkparse
